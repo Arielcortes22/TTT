84aa40ffd0466c79ca8204217ddb4da201e6f3a0
Ariel Cortes22 <arichango0107@gmail.com>
2025-02-11 13:21:20 -0600

    mi segundo commit

--- src/Js/toc.js
+++ src/Js/toc.js
@@ -16,8 +16,22 @@ function userMove(e) {
         checkline(6, 7, 8);
         checkline(0, 3, 6);
         checkline(1, 4, 7);
-        checkline();
-        checkline();
-        checkline();
+        checkline(2, 5, 8);
+        checkline(0, 4, 8);
+        checkline(6, 4, 2);
     }
 }
+function checkline(c1, c2, c3) {
+    if(
+        Cats[c1].innerHTML.length &&
+        Cats[c1].innerHTML === Cats[c2].innerHTML &&
+        Cats[c2].innerHTML === Cats[c3].innerHTML 
+      
+    ){
+        showWinner(Cats[c1].innerHTML);
+    }
+}
+
+function showWinner(player) {
+    document.querySelector("#Resultado").innerHTML = player + "win"
+} 
