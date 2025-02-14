a91623567c8b9ae07ae44692f4cdef9289950f40
Ariel Cortes22 <arichango0107@gmail.com>
2025-02-14 11:36:42 -0600

    cuarto dia

--- src/Js/toc.js
+++ src/Js/toc.js
@@ -5,17 +5,37 @@ let Cats = document.getElementsByClassName("Cat");
 
 for (let index = 0; index < Cats.length; index++) {
 Cats[index].addEventListener("click",function () {
-    console.log(Cats[index]);
-    Cats[index].innerHTML = "x"
-    selector()
-    
+        if (Cats[index].textContent!="") {
+                console.log("No puedes");
+                alert ("No puedes");
+                
+        } else{
+            console.log(Cats[index]);
+            Cats[index].textContent = "x"
+            selector()
+        }
+     
 });
   
 }
 function selector() {
-    let Bot = Math.floor((Math.random() * 9));
-    console.log(Bot);
-    Cats[Bot].innerHTML = "o"
+
+    let CatsVacias=[];
+    for(let index=0; index<Cats.length; index++) {
+        if(Cats[index].textContent == "" ){
+            CatsVacias.push(index);
+        }
+    }
+
+    if (CatsVacias.length > 0) {
+        const Catscons =
+        CatsVacias[ Math.floor(Math.random()* CatsVacias.length)];
+        Cats[Catscons].innerHTML = "o"
+        
+  
+      
+    }
+ 
                
         checkline(0, 1, 2);
         checkline(3, 4, 5);
@@ -41,15 +61,26 @@ function checkline(c1, c2, c3) {
 
 function showWinner(player) {
     document.getElementById("Resultado").innerHTML = player + "WIN"
+
+    localStorage.setItem("winner", player);
 } 
 
     Reiniciar.addEventListener("click", function() {
 
     for (let index = 0; index < Cats.length; index++) {
         Cats[index].textContent = "";
+        document.getElementById("Resultado").innerHTML = "";
         console.log(Reiniciar);
     }  
+    localStorage.removeItem("winner");
     })
+
+    window.onload = function() {
+        const winner = localStorage.getItem("winner");
+        if (winner) {
+            document.getElementById("Resultado").innerHTML = winner + " WIN";
+        }
+    }
 
     
 
